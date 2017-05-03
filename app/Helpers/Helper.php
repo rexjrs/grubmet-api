b81551c2c31597d10e37923d65c930ff781cc876
Thomas Charlesworth <thomas.charlesworths@gmail.com>
2017-05-04 00:22:41 +0700

    test

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,20 +1,8 @@
 <?php
 
-namespace App;
-
-class Helper extends Controller
+class Helpers
 {
     public function generateRandomString($length = 60) {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
-    }
-
-    public function mealID($length = 10) {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $charactersLength = strlen($characters);
         $randomString = '';
--- app/Http/Controllers/Address.php
+++ app/Http/Controllers/Address.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Address;
 use App\Users;
 use App\Tokens;
-use App\Helper;
 
 class Address extends Controller
 {
@@ -15,6 +14,46 @@ class Address extends Controller
     public function __construct()
     {
         $this->authkey = 'jkh89sdf87bjkrgknl234jksdf09sdkl235lksaf90safkjl23';
+    }
+
+    
+    public function generateRandomString($length = 60) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    public function checkToken($user_id, $token){
+        $checkToken = Tokens::where('user_id', $user_id)->where('token',$token)->first();
+        if(!$checkToken){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public function checkAPIKey($request){
+        if(!isset($request->headers->all()["authorization"])){
+            return false;
+        }else if(str_replace(['"','[',']'], "", json_encode($request->headers->all()["authorization"])) != $this->authkey){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public function checkUserToken($request){
+        if(!isset($request->headers->all()['token'])){
+            return false;
+        }else if($this->checkToken($user_id, str_replace(['"','[',']'], "", json_encode($request->headers->all()["token"])))){
+            return false;
+        }else{
+            return true;
+        }
     }
 
     // ============================= Base API Structure =============================
